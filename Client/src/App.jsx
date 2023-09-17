26c2e78026914d7a09c6ebf4ec22b8b6a9cd83b3
Muhammad Burhan Ahmed <burhan.ahmed60090@gmail.com>
2023-09-17 16:26:43 +0500

    Updates

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,55 @@
-import {io} from 'socket.io-client';
-const socket = io();
+import { io } from 'socket.io-client';
+import React, { useState, useEffect } from 'react';
+
+const socket = io('http://localhost:8000');
+
 
 export default function App() {
 
+  const [message, setMessage] = useState('');
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    socket.on('message', (msg) => {
+      setHistory((prevMessages) => [...prevMessages, { text: msg, type: 'received' }]);
+      console.log('Received message:', msg);
+
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (message.trim() !== '') {
+      socket.emit('message', message);
+      setHistory((prevMessages) => [...prevMessages, { text: message, type: 'sent' }]);
+      console.log('Message sent:', message);
+      setMessage('');
+    }
+  };
   return (
     <>
       <h1>Chat App</h1>
-      <div className="border">
-        <input type="text" onClick={(e) => { e.target.value }} className="p-2 border rounded-md" placeholder="Enter Message..." />
-        <button type="button" >
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="p-2 border rounded-md"
+          placeholder="Enter Message..."
+        />
+        <button type="button" onClick={sendMessage}>
           SEND
         </button>
+      </div>
+      <div className="messages">
+        {history.map((msg, index) => (
+          <div key={index} className={`message ${msg.type}`}>
+            {msg.text}
+          </div>
+        ))}
       </div>
     </>
   )
--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,17 @@ import { Server } from 'socket.io';
 const app = express(); // Create an Express.js application
 const server = http.createServer(app);
 app.use(cors());
-const io = new Server(server) //express server created inside socket instance
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5000', // Replace with your frontend's URL
+        methods: ['GET', 'POST'], // Allow the necessary HTTP methods
+        credentials: true, // Allow cookies and credentials
+    },
+});
 
 app.get('/', (req, res) => {
     console.log("Running")
-    res.sendFile(new URL('D:/ChatApp/Client/index.html', import.meta.url).pathname);
+    res.sendFile('D:/ChatApp/Client/index.html');
 });
 
 io.on("connection", (socket) => {
@@ -21,10 +27,10 @@ io.on("connection", (socket) => {
         console.log('User disconnected');
     });
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+        console.log('Message received and broadcasted:', msg);
     });
 })
-
 
 server.listen(8000, () => console.log("Server is running now"));
